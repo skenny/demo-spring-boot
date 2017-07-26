e358efac25a8db87ad239c4f518b18a063f309c1
Shaun Kenny <skenny@gmail.com>
2017-07-26 14:26:19 -0300

    updated thymeleaf spring-security extras; enabled SpringSecurityDialect; wrapped Logout link in authenticated check

--- src/main/java/com/example/demo/ThymeleafConfiguration.java
+++ src/main/java/com/example/demo/ThymeleafConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
@@ -9,13 +10,23 @@ import nz.net.ultraq.thymeleaf.LayoutDialect;
 public class ThymeleafConfiguration {
 
 	/**
-	 * Enables layout dialect
+	 * Enables layout dialect.
 	 * 
 	 * https://github.com/ultraq/thymeleaf-layout-dialect
 	 */
 	@Bean
 	public LayoutDialect layoutDialect() {
 		return new LayoutDialect();
+	}
+
+	/**
+	 * Enables spring-security extras.
+	 * 
+	 * https://github.com/thymeleaf/thymeleaf-extras-springsecurity
+	 */
+	@Bean
+	public SpringSecurityDialect springSecurityDialect() {
+		return new SpringSecurityDialect();
 	}
 
 }
