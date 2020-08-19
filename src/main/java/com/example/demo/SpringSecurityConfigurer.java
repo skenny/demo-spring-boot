f2e097a82dcd67834f7eec584729496aaa2bc15f
Shaun Kenny <skenny@gmail.com>
2020-08-19 15:41:09 -0300

    updated gradle, spring-boot, etc

--- src/main/java/com/example/demo/SpringSecurityConfigurer.java
+++ src/main/java/com/example/demo/SpringSecurityConfigurer.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -10,6 +11,11 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 	@Autowired private AccessDeniedHandler accessDeniedHandler;
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/css/**", "/img/**", "/js/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/example/demo/ThymeleafConfiguration.java
+++ src/main/java/com/example/demo/ThymeleafConfiguration.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
